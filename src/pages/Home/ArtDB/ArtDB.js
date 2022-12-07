da8d97972043037866c2a3c988753926c2d1df13
juyoung810 <kjy2967@naver.com>
2022-12-07 16:19:53 +0900

    FEAT : create Art database section in main

--- src/pages/Home/ArtDB/ArtDB.js
+++ src/pages/Home/ArtDB/ArtDB.js
@@ -5,7 +5,39 @@ const ArtDB = () => {
   return (
     <section className="artbase">
       <SectionHeader title="Art Database" line="right-line" />
-      <div className="artbase-01"></div>
+      <div className="artbase__item">
+        <img src={require("../../../assets/art_01.png")}></img>
+        <div className="artbase__item__info">
+          <a href="#" className="artbase__info__deatil">
+            <span className="artbase__info-name">Art Database</span>
+            <h3>Artist</h3>
+            <h5>큐아트 작가 정보 바로가기</h5>
+            <span className="artbase-link"></span>
+          </a>
+        </div>
+      </div>
+      <div className="artbase__item">
+        <div className="artbase__item__info">
+          <a href="#" className="artbase__info__deatil right">
+            <span className="artbase__info-name">Art Database</span>
+            <h3>Work</h3>
+            <h5>큐아트 작품 정보 바로가기</h5>
+            <span className="artbase-link"></span>
+          </a>
+        </div>
+        <img src={require("../../../assets/art_02.png")}></img>
+      </div>
+      <div className="artbase__item">
+        <img src={require("../../../assets/art_03.png")}></img>
+        <div className="artbase__item__info">
+          <a href="#" className="artbase__info__deatil">
+            <span className="artbase__info-name">Art Database</span>
+            <h3>Venue</h3>
+            <h5>전시기관 정보 바로가기</h5>
+            <span className="artbase-link"></span>
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
